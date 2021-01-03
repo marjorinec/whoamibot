ad2f05ae8313021f53cb9a65b6035ca6b8f58aa0
Alline Marjorie <allinemarjorie@gmail.com>
2021-01-02 23:14:46 -0300

    feat: start telegram bot with express, axios and body-parser

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.post("/new-message", function (req, res) {
+  const { message } = req.body;
+
+  if (!message) {
+    return res.end();
+  }
+
+  axios
+    .post(
+      "https://api.telegram.org/bot1234111483:AAEUbW5tSvPkHqOAsM2mP312UsR3eC8RtMg/sendMessage",
+      {
+        chat_id: message.chat.id,
+        text: "Hey!! My cat is gorgeous!!!!",
+      }
+    )
+    .then((response) => {
+      console.log("Message posted");
+      res.end("ok");
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+      res.end("Error: ", err);
+    });
+});
+
+app.listen(3000, function () {
+  console.log("Who Am I Bot listening on port 3000!");
+});
